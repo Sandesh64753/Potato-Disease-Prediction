7352c503ae9c6555cf42fa2423ccfcddc7569fed
Sandesh Vairagar <vairagarsandesh45@gmail.com>
2026-05-23 20:32:28 +0530

    Update CORS origins to include production URL

--- api/main.py
+++ api/main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 
 origins = [
     "http://localhost",
-    "http://localhost:5173",
+    "https://potato-disease-prediction-frontend.onrender.com/",
 ]
 app.add_middleware(
     CORSMiddleware,
